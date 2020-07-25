693f8f6c71b5ab25fccc506b880addf99751b180
ZuguangTong <810732627@qq.com>
2020-07-25 18:16:46 +0800

    防抖函数

--- JavaScript/UtilFunc.js
+++ JavaScript/UtilFunc.js
@@ -3,46 +3,85 @@
  * @param {*} fn 要柯里化的函数
  */
 function curry(fn,args){
-    var length = fn.length;
-    var args = args||[];
+    var length = fn.length; //3
+    var args =args || [];//[]
     return function(){
         var innerArgs = Array.prototype.slice.call(arguments);
         var finalArgs = args.concat(innerArgs);
-
-        if(finalArgs.length<length){
+        if (finalArgs.length<length) {
             return curry.call(this,fn,finalArgs);
-        }else{
+        } else {
             return fn.apply(this,finalArgs);
         }
-        
     }
-}
-// test
-// function add(num1,num2,num3){
-//     return num1+num2+num3;
-// }
 
-// var curriedAdd = curry(add);
-// console.log(curriedAdd(3)(4,5));
+}
+
+// test
+function add(num1,num2,num3){
+    return num1+num2+num3;
+}
+
+var curriedAdd = curry(add);
+console.log(curriedAdd(3)(4,5));
 
 
 /**
  * 函数节流
  * @func 函数
  * @time 定时器时间
- * @context 执行上下文
  */
-function throttle(func,time,context){
-    clearTimeout(func.tId);
-    func.tId = setTimeout(function(){
-        func.call(context);
-    },time);
+function throttle(func,time){
+    var tid =null;
+    return function(){
+        let context = this;
+        let args = arguments;
+        if(!tid){
+            tid = setTimeout(
+                ()=>{
+                    tid = null;func.apply(context,args);
+                },time
+            )
+        }
+    }
 }
-// function resizeDiv(){
-//     var throttleTry = document.querySelector(".throttleTry");
-//     console.log(throttleTry);
-//     throttleTry.getElementsByClassName.height = throttleTry.offsetWidth+"px";
-// }
-// window.onresize = function(){
-//     this.throttle(resizeDiv,500);
-// }
+/**
+ * 防抖
+ * @param {*} func 
+ * @param {*} time 
+ * @param {*} immediate 是否立即执行
+ */
+function debounce(func,time,immediate){
+    let timer;
+    return function(){
+        let context = this;
+        let args = arguments;
+        if(timer){
+            clearTimeout(timer);
+        }
+
+        if (immediate) {
+            let callNow = !timer;
+            timer = setTimeout(() => {
+                timer = null;
+            }, wait);
+            if(callNow) {
+                func.apply(context,args)
+            };
+        } else {
+            timer = setTimeout(() => {
+                func.apply(this, args)
+              }, time)
+        }
+
+    }
+}
+
+function resizeDiv(){
+    var throttleTry = document.querySelector(".throttleTry");
+    console.log(throttleTry);
+    throttleTry.getElementsByClassName.height = throttleTry.offsetWidth+"px";
+}
+window.onresize = throttle(resizeDiv,3000);
+
+
